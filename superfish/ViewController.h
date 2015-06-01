bca68a4f3eba1ed5c6e8b26eb3ad0a09a2663d3c
ziyadparekh <ziyad@1stdibs.com>
2015-06-01 13:47:49 -0400

    Initial Commit

---
+++ superfish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  superfish
+//
+//  Created by Ziyad Parekh on 6/1/15.
+//  Copyright (c) 2015 Ziyad Parekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ superfish/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  superfish
+//
+//  Created by Ziyad Parekh on 6/1/15.
+//  Copyright (c) 2015 Ziyad Parekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
